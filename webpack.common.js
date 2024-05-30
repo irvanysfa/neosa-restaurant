1452cbafb00ed8abe9eb710d9dab83372e426331
irvanysfa <129833141+irvanysfa@users.noreply.github.com>
2024-05-30 23:49:07 +0700

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,5 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { GenerateSW } = require('workbox-webpack-plugin');
-const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
-const ImageminMozjpeg = require('imagemin-mozjpeg');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
 module.exports = {
+  mode: 'production', // Tambahkan mode produksi di sini
   entry: {
     app: './src/scripts/index.js',
     detailrestaurant: './src/scripts/detailrestaurant.js',
@@ -65,56 +57,7 @@ module.exports = {
       clientsClaim: true,
       skipWaiting: true,
       runtimeCaching: [
-        {
-          urlPattern: new RegExp("https://restaurant-api.dicoding.dev"),
-          handler: "StaleWhileRevalidate",
-          options: {
-            cacheName: "api-cache",
-            expiration: {
-              maxEntries: 100,
-            },
-          },
-        },
-        {
-          urlPattern: new RegExp("https://restaurant-api.dicoding.dev/detail"),
-          handler: "StaleWhileRevalidate",
-          options: {
-            cacheName: "restaurant-detail-cache",
-            expiration: {
-              maxEntries: 50,
-            },
-          },
-        },
-        {
-          urlPattern: /\.(?:js|css|scss|html)$/,
-          handler: "StaleWhileRevalidate",
-          options: {
-            cacheName: "static-resources",
-          },
-        },
-        {
-          urlPattern: new RegExp("detailrestaurant.html"),
-          handler: "NetworkFirst",
-          options: {
-            cacheName: "detail-page-cache",
-            expiration: {
-              maxEntries: 5,
-            },
-          },
-        },
-        {
-          urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
-          handler: "CacheFirst",
-          options: {
-            cacheName: "images-cache",
-            expiration: {
-              maxEntries: 50,
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
+        // Konfigurasi runtime caching
       ],
     }),
     new ImageminWebpackPlugin({
